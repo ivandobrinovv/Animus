aa045015f04c85c015b6e97d70370adf376da4e0
Ivan Peev <idobrinovv@gmail.comivan>
2022-02-22 10:49:53 +0200

    Add User Repository and Interface

---
+++ Animus.Data/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Animus.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animus.Data.Repositories.Interfaces
+{
+    internal interface IUserRepository : IBaseRepository<User>
+    {
+    }
+}
---
+++ Animus.Data/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Animus.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animus.Data.Repositories
+{
+    public class UserRepository : BaseRepository<User>
+    {
+        public UserRepository(AnimusDbContext context) : base(context)
+        {
+        }
+
+        
+    }
+}
