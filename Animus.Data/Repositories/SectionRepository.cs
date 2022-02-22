293227957e9456be88e0158ae5077bb5966c663f
Ivan Peev <idobrinovv@gmail.comivan>
2022-02-22 10:34:43 +0200

    Add Section repository

--- Animus.Data/Repositories/SectionRepository.cs
+++ Animus.Data/Repositories/SectionRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Animus.Data.Entities;
 using Animus.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Animus.Data.Repositories
 {
@@ -7,7 +9,32 @@ namespace Animus.Data.Repositories
     {
         public SectionRepository(AnimusDbContext context) : base(context)
         {
-
+            
         }
+
+        public override async ValueTask<Section?> GetById(Guid id)
+        {
+            return await context.Sections
+                .Include(x => x.Posts)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            
+        }
+        
+        public override async Task<ICollection<Section>> GetAll()
+        {
+            return await context.Sections
+                .Include(x => x.Posts)
+                .ToListAsync();
+        }
+        public override async Task<ICollection<Section>> GetAll(Expression<Func<Section, bool>> filter)
+        {
+            return await context.Sections
+                .Include(x => x.Posts)
+                .Where(filter)
+                .ToListAsync();
+        }
+
+        
+
     }
 }
